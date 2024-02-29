545e333f34eae920a22cee280699e52d044c9a82
Syarif221106 <161261198+Syarif221106@users.noreply.github.com>
2024-02-29 08:01:29 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,46 +1,71 @@
 <?php
-include "header.php";
-include "navbar.php";
+session_start();
+
+if(isset($_SESSION['level'])){
+    if($_SESSION['level'] == "1"){
+    header("location:administrator/index.php");
+    }
+    if($_SESSION['level'] == "2"){
+        header("location:petugas/index.php");
+        }
+}
+
 ?>
-<div class="card mt-2">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="card">
-                    <div class="card-body">
-                        Data Barang 
-                        <?php
-                        include '../koneksi.php';
-                        $data_produk = mysqli_query($koneksi, "SELECT * FROM produk");
-                        $jumlah_produk = mysqli_num_rows($data_produk);
-                        ?>
-                        <h3><?php echo $jumlah_produk; ?></h3>
-                        <a href="data_barang.php" class="btn btn-outline-primary btn-sm">Detail</a>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>UKK KASIR</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+     <link rel="shortcut icon" href="../assets/login.png" type="image/x-icon">
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="card mt-5">
+                <div class="row">
+               
+                    <div class="col-6">
+                        <div class="card-body">
+                            <p class="text-center mt-5">Silahkan Masukan Username dan Password</p>
+                            <?php
+                            if(isset($_GET['pesan'])){
+                                if($_GET['pesan'] == "gagal"){
+                                    echo "<div class='alert'>Username dan Password Tidak Sesuai</div>";
+                                }
+                            }
+                            ?>
+                            <form action="cek_login.php" method="post">
+                                <div class="form-group">
+                                    <label>Username</label>
+                                    <input type="text" class="form-control" name="username">
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" name="password">
+                                </div>
+                                <div class="form-group">
+                                    <button class="btn btn-primary form-control mt-5" type="submit">Login</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="card">
-                    <div class="card-body">
-                        Data pembelian 
-                        <?php
-                        include '../koneksi.php';
-                        $data_penjualan = mysqli_query($koneksi, "SELECT * FROM penjualan");
-                        $jumlah_penjualan = mysqli_num_rows($data_penjualan);
-                        ?>
-                        <h3><?php echo $jumlah_penjualan; ?></h3>
-                        <a href="pembelian.php" class="btn btn-outline-primary btn-sm">Detail</a>
+                     <div class="col-6">
+                        <div class="card-body">
+                            <img src="assets/login.png" width="500">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="card mt-2">
-    <div class="card-body">
-        <p>Selamat datang dihalaman petugas, silahkan anda bisa mengakses beberapa fitur</p>
-    </div>
-</div>
-<?php
-include "footer.php";
-?>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" 
+integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" 
+integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
